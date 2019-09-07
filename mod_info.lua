f812c4ef44449716b3352b5e7d13f153b5f2abfd
CodingSquirrel <13072675+CodingSquirrel@users.noreply.github.com>
2019-09-07 09:08:23 -0400

    Initial Commit, v4.1 from the mod vault

---
+++ mod_info.lua
@@ -0,0 +1,8 @@
+name = "Selection Deprioritizer V4.1"
+version = 4.1
+copyright = "nah"
+description = "Prevents accidental selection of things like mobile T3 artillery. Reduces selection to domains. Deselects assisting shields and scouts. Edited by SpoCk0nd0pe."
+author = "partytime"
+uid = "d2e4b420-339a-4766-9293-681b27f92ef8"
+exclusive = false
+ui_only =true
