f6580bc65d171739f2ee9e47c5ce8d142b9452de
Washeh <13085405+Washeh@users.noreply.github.com>
2019-12-01 22:12:01 +0100

    ingame options for filters via hotkeys, saved in game prefs (#4)

--- mod_info.lua
+++ mod_info.lua
@@ -1,8 +1,8 @@
 name = "Selection Deprioritizer"
-version = 5
+version = 6
 copyright = "nah"
 description = "Prevents accidental selection of things like mobile T3 artillery. Reduces selection to domains. Deselects assisting shields and scouts. Edited by SpoCk0nd0pe and CodingSquirrel."
-author = "partytime"
-uid = "e894c451-056a-46a1-90bf-54e6e083528b"
+author = "partytime, changes by CodingSquirrel and Myxir"
+uid = "bc7731b6-1416-11ea-8d71-362b9e155667"
 exclusive = false
 ui_only =true
